66548da40c5b66492d80b1c34223d4d860b67d92
Peter Kehl <peter.kehl@gmail.com>
2022-07-16 07:56:06 -0700

    Removed HashedSet. Impl for std HashSet instead.

--- src/hash/set/mod.rs
+++ src/hash/set/mod.rs
@@ -1,15 +1,9 @@
 use crate::set::Set;
 use core::hash::Hash;
-use slicing::abstra::NewEmptyLike;
 use std::collections::{hash_set, hash_set::Iter, HashSet};
 
-#[derive(Debug, Clone)]
-pub struct HashedSet<T> {
-    set: HashSet<T>,
-}
-
-impl<T: Hash + Eq + Clone> Set<T> for HashedSet<T> {
-    type ITER<'a> = HashedSetIter<'a, T>
+impl<T: Hash + Eq + Clone> Set<T> for HashSet<T> {
+    type ITER<'a> = HashSetIter<'a, T>
     where
         T: 'a,
         Self: 'a,
@@ -22,17 +16,17 @@ impl<T: Hash + Eq + Clone> Set<T> for HashedSet<T> {
     ;
 
     fn contains(&self, value: &T) -> bool {
-        self.set.contains(value)
+        self.contains(value)
     }
     fn insert(&mut self, value: T) -> bool {
-        self.set.insert(value)
+        self.insert(value)
     }
     fn remove(&mut self, value: &T) -> bool {
-        self.set.remove(value)
+        self.remove(value)
     }
     fn iter<'a>(&'a self) -> Self::ITER<'a> {
-        HashedSetIter {
-            set_iter: self.set.iter(),
+        HashSetIter {
+            set_iter: self.iter(),
         }
     }
 
@@ -41,47 +35,18 @@ impl<T: Hash + Eq + Clone> Set<T> for HashedSet<T> {
     }
 
     fn iter_ref<'a>(&'a self) -> Self::ITERREF<'a> {
-        self.set.iter()
-    }
-}
-impl<T: Hash + Eq + Clone> NewEmptyLike for HashedSet<T> {
-    fn new_empty_like(&self) -> Self {
-        Self {
-            set: HashSet::<T>::new(),
-        }
+        self.iter()
     }
 }
 
-impl<T: Hash + Eq> HashedSet<T> {
-    pub fn new() -> Self {
-        Self {
-            set: HashSet::new(),
-        }
-    }
-    pub fn new_with(set: HashSet<T>) -> Self {
-        Self { set }
-    }
-}
-
-pub struct HashedSetIter<'a, T: 'a> {
+pub struct HashSetIter<'a, T: 'a> {
     set_iter: hash_set::Iter<'a, T>,
 }
 
-impl<'a, T: Clone> Iterator for HashedSetIter<'a, T> {
+impl<'a, T: Clone> Iterator for HashSetIter<'a, T> {
     type Item = T;
     #[inline]
     fn next(&mut self) -> Option<T> {
         self.set_iter.next().cloned()
-    }
-}
-
-impl<T: core::hash::Hash + Eq> FromIterator<T> for HashedSet<T> {
-    fn from_iter<IT>(iter: IT) -> Self
-    where
-        IT: IntoIterator<Item = T>,
-    {
-        Self {
-            set: HashSet::from_iter(iter),
-        }
     }
 }
